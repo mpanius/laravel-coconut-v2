b68caa78f2d928d211455f64852b56f8970a287f
Nidhal Abidi <nidhalkratos@gmail.com>
2021-12-18 02:03:55 +0100

    Fix bug in webhook route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Nidhalkratos\LaravelCoconut\Events\CoconutNotificationReceived;
 
-Route::get('/coconut/callback/{videoId}', function($videoId, Request $request){
+Route::post('/coconut/callback/{videoId}', function($videoId, Request $request){
     $body = $request->getContent();
     $webhook = json_decode($body, true);
     event(new CoconutNotificationReceived($videoId,$webhook));
